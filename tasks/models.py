624c6f8fa0e866bb9da323f9c578cf8dd152eb88
Megan Paquin <106716810+meganpaquin@users.noreply.github.com>
2022-11-16 22:14:10 -0500

    Add a dependent model for tasks
    Add a status model that is a dependent of the tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -3,6 +3,14 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
+class Status(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+        
 class Task(models.Model):
     task_name = models.CharField(max_length=120)
     task_summary = models.TextField()
@@ -19,7 +27,13 @@ class Task(models.Model):
         on_delete=models.CASCADE,
         related_name="task_author"
     )
-
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.CASCADE,
+        blank = True,
+        null = True
+    )
+    
     # need to add status
     # need to add risks / issues
     # need to add Project
