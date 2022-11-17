eac290446601dc4f42818c7e2f69efcd7d162e2a
Megan Paquin <106716810+meganpaquin@users.noreply.github.com>
2022-11-16 21:12:17 -0500

    Create templates
    Create views for pages and create base.css. Add css from previous build and edit for Django compatibility

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,10 @@
 from django.views.generic import TemplateView
 
-class HomePageView(TemplateView):
+class IndexPageView(TemplateView):
     template_name = "pages/index.html"
+
+class HomePageView(TemplateView):
+    template_name = "pages/home.html"
+
+class DashboardPageView(TemplateView):
+    template_name = "pages/dashboard.html"
