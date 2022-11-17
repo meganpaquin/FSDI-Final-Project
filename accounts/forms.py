0c1cd400ecdc82f967b55d18158200c89f536883
Megan Paquin <106716810+meganpaquin@users.noreply.github.com>
2022-11-16 19:03:12 -0500

    Change User Model
    Add email, first name, and last name to the sign up page

---
+++ accounts/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(max_length=254, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
