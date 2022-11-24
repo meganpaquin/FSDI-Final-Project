20b950fbff5a6bf7508c1dba031c66b3b8b3def1
Megan Paquin <106716810+meganpaquin@users.noreply.github.com>
2022-11-23 22:46:16 -0500

    create list page

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, IndexPageView, DashboardPageView
+from .views import HomePageView, IndexPageView, DashboardPageView, ListPageView
 
 urlpatterns = [
     path('', IndexPageView.as_view(), name='index'),
     path('home', HomePageView.as_view(), name="home"),
     path('dashboard', DashboardPageView.as_view(), name="dashboard"),
+    path('list', ListPageView.as_view(), name="list"),
 ]
