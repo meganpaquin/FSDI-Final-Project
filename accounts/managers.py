b4822eed5e07ee7fdbebb392cd61b412f1e96d37
Megan Paquin <106716810+meganpaquin@users.noreply.github.com>
2022-12-05 23:15:03 -0500

    project back on track

--- accounts/managers.py
+++ accounts/managers.py
@@ -7,28 +7,28 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
-    def create_user(self, email, password, first_name, last_name, phone, image, color):
+    def create_user(self, email, password, **extra_fields):
         """
         Create and save a User with the given email and password.
         """
         if not email:
             raise ValueError(_('The Email must be set'))
         email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, last_name=last_name, phone=phone, color=color, image=image)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, first_name, last_name, phone, color, image):
+    def create_superuser(self, email, password, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
         """
-        email.setdefault('is_staff', True)
-        email.setdefault('is_superuser', True)
-        email.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
 
-        if email.get('is_staff') is not True:
+        if extra_fields.get('is_staff') is not True:
             raise ValueError(_('Superuser must have is_staff=True.'))
-        if email.get('is_superuser') is not True:
+        if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email, password, first_name, last_name, phone, color, image)
+        return self.create_user(email, password, **extra_fields)
--- tasks/admin.py
+++ tasks/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Status, Priority
 
 admin.site.register(Task)
+admin.site.register(Status)
+admin.site.register(Priority)
